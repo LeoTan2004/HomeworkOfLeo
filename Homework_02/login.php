0a9e13cc0c070c58cfb4f2c705eeffb87a6e2e52
LeoTan2004 <113982129+LeoTan2004@users.noreply.github.com>
2022-11-06 16:10:50 +0800

    Update login.php

--- Homework_02/login.php
+++ Homework_02/login.php
@@ -8,13 +8,12 @@ function get_rand_str($lenth){
     return $str;
 }
 $name = $_POST["id"];
-$nickName = $_POST["NickName"];
+$pwd = $_POST["pwd"];
 $token="";
-if (strcmp($name,"LeoTan")==0&& strcmp($nickName,"Leon")==0){
+if (strcmp($name,"LeoTan")==0&& strcmp($pwd,"Leon")==0){//实际中可能要在数据库中完成的查询操作
     $token = get_rand_str(50);
-    echo "{ token:".$token."}";
+    echo "{ token:".$token."}";//生成后应该用数据库储存起来，存储时记得需要记录时间、Mac地址等关键信息，保障信息安全
 }else{
-    echo $nickName,$name;
     echo "wrong answer";
 }
 ?>
